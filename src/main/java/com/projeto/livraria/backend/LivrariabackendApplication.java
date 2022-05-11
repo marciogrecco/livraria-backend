2b61c30113728c8931c5aa946ecd39fe7fbdbcfe
unknown <marcio.greco@outlook.com>
2022-05-11 08:26:09 -0300

    Project created

---
+++ src/main/java/com/projeto/livraria/backend/LivrariabackendApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.livraria.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariabackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/livraria/backend/LivrariabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.livraria.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
